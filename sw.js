a179853e28e49ca6eaad851f0278bebfde1d54a4
nuwandazl <repinazlata@mail.ru>
2022-11-11 09:29:54 +0500

    activate

--- sw.js
+++ sw.js
@@ -1,26 +1,37 @@
 const staticAssets=[
-    './',
-    './index.html',
-    './img/калькул.jpg',
-    './images/icons/icon-128x128.png',
-    './images/icons/icon-192x192.png',
-    './css/bootstrap.min.css',
-    './js/bootstrap.bundle.min.js',
-    './app.js'
+    '.',
+    'index.html',
+    'img/калькул.jpg',
+    'images/icons/icon-128x128.png',
+    'images/icons/icon-192x192.png',
+    'css/bootstrap.min.css',
+    'js/bootstrap.bundle.min.js',
+    'app.js'
 ]
 const staticCashName = 'site-static-v1'
 
 // INSTALL
 self.addEventListener('install', async evt => {
-    const cache = await caches.open(staticCashName)
-    await cache.addAll(staticAssets)
-    console.log('INSTALL')
+    self.addEventListener('install',evt =>{
+        evt.waitUntil(
+            caches.open(staticCacheName).then((cache) => {
+                console.log('Кэширование ресурсов')
+                cache.addAll(assets)
+            })
+        )
+    })
 })
 
 // ACTIVATE
 
 self.addEventListener('activate', evt => {
-    console.log('ACTIVATE')
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName))
+                .map(key => caches.delete(key))
+        })
+    )
 })
 
 // FETCH
